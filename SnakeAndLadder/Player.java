2128aa8114ee77ebf8b49ecc60b89e6602d30cc2
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-03-09 00:15:49 +0530

    changes

---
+++ SnakeAndLadder/Player.java
@@ -0,0 +1,11 @@
+package SnakeAndLadder;
+
+public class Player {
+    int id;
+    int currentPositon;
+
+    Player(int id,int currentPositon) {
+        this.id=id;
+        this.currentPositon=currentPositon;
+    }
+}
