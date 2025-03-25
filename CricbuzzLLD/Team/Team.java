b4bcdf71d2adc881678e4c571a0f6d72cc1e6561
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-03-25 11:14:39 +0000

    Initial commit

---
+++ CricbuzzLLD/Team/Team.java
@@ -0,0 +1,10 @@
+package LowLevelDesign.CricbuzzLLD.Team;
+import java.util.*;
+
+public class Team {
+    public String teamName;
+    Queue<PlayerDetails> playing11;
+    
+    Player captain;
+
+}
